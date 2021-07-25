8f11e8d50e1b620a0448730105b2dba0bf9debc0
katredford <katharine.redford@gmail.com>
2021-07-25 14:07:26 -0500

    adding importing mutations to all forms, problem with @apollo atm

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,7 +7,7 @@ import {
 	Button,
 } from "react-bootstrap";
 
-import { useQuery, useMutation } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 
 import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from "../utils/mutations";
